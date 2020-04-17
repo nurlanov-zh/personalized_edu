3b2743d05a8f7b6144a16b0e7ffbfc88b3612a95
Nurlanov Zhakshylyk <jaksha97@mail.ru>
2020-04-18 04:41:42 +0600

    Experimanted with HeadPoseEstimation, added Euler2Rotation_matrix function

--- 3d_head_pose_estimation.py
+++ 3d_head_pose_estimation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-#The MIT License (MIT)
-#Copyright (c) 2016 Massimiliano Patacchiola
+# The MIT License (MIT)
+# Copyright (c) 2016 Massimiliano Patacchiola
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # In this example the Deepgaze CNN head pose estimator is used to get the YAW angle.
 # The angle is projected on the input images and showed on-screen as a red line.
@@ -19,88 +19,110 @@ import tensorflow as tf
 # tf.disable_v2_behavior()
 import cv2
 from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
-
-#Function used to get the rotation matrix
-def yaw2rotmat(yaw):
-    x = 0.0
-    y = 0.0
-    z = yaw
-    ch = np.cos(z)
-    sh = np.sin(z)
-    ca = np.cos(y)
-    sa = np.sin(y)
-    cb = np.cos(x)
-    sb = np.sin(x)
-    rot = np.zeros((3,3), 'float32')
-    rot[0][0] = ch * ca
-    rot[0][1] = sh*sb - ch*sa*cb
-    rot[0][2] = ch*sa*sb + sh*cb
-    rot[1][0] = sa
-    rot[1][1] = ca * cb
-    rot[1][2] = -ca * sb
-    rot[2][0] = -sh * ca
-    rot[2][1] = sh*sa*cb + ch*sb
-    rot[2][2] = -sh*sa*sb + ch*cb
-    return rot
+from Rotations import yaw2rotmat, euler2mat
 
 
-sess = tf.Session() #Launch the graph in a session.
-my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+sess = tf.Session()  # Launch the graph in a session.
+my_head_pose_estimator = CnnHeadPoseEstimator(sess)  # Head pose estimation object
 # Load the weights from the configuration folders
-my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"))
-my_head_pose_estimator.load_roll_variables(os.path.realpath("../../etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
-my_head_pose_estimator.load_pitch_variables(os.path.realpath("../../etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
+cwd = os.path.abspath(os.path.dirname(__file__))
 
-for i in range(1,9):
-    file_name = str(i) + ".jpg"
-    file_save = str(i) + "_axes.jpg"
-    print("Processing image ..... " + file_name)
-    #file_name = "1.jpg"
-    image = cv2.imread(file_name)
+# my_head_pose_estimator.load_yaw_variables(os.path.realpath("../../etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"))
+# my_head_pose_estimator.load_roll_variables(os.path.realpath("../../etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"))
+# my_head_pose_estimator.load_pitch_variables(os.path.realpath("../../etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"))
+
+model_path_yaw = "third_party/deepgaze-2.0/etc/tensorflow/head_pose/yaw/cnn_cccdd_30k.tf"
+model_path_roll = "third_party/deepgaze-2.0/etc/tensorflow/head_pose/roll/cnn_cccdd_30k.tf"
+model_path_pitch = "third_party/deepgaze-2.0/etc/tensorflow/head_pose/pitch/cnn_cccdd_30k.tf"
+
+my_head_pose_estimator.load_yaw_variables(os.path.abspath(os.path.join(cwd, model_path_yaw)))
+my_head_pose_estimator.load_roll_variables(os.path.abspath(os.path.join(cwd, model_path_roll)))
+my_head_pose_estimator.load_pitch_variables(os.path.abspath(os.path.join(cwd, model_path_pitch)))
+
+webcam = cv2.VideoCapture(0)
+resize = True
+while True:
+    # We get a new frame from the webcam
+    _, image = webcam.read()
+    print(image.shape)
     cam_w = image.shape[1]
     cam_h = image.shape[0]
+
+    # TODO: Crop face using dlib as face detector
+    # TODO: Test better the visualization
+
+    min_shape = max(min(cam_h, cam_w), 64)
+
+    # resizing
+    if resize:
+        (cam_w, cam_h) = (min_shape, min_shape)
+        image = cv2.resize(image, (cam_w, cam_h), interpolation=cv2.INTER_AREA)
+    else:
+        # crop
+        delta_w = max(cam_w - min_shape, 0)
+        delta_h = max(cam_h - min_shape, 0)
+
+
+
     c_x = cam_w / 2
     c_y = cam_h / 2
-    f_x = c_x / np.tan(60/2 * np.pi / 180)
+    f_x = c_x / np.tan(60 / 2 * np.pi / 180)
     f_y = f_x
     camera_matrix = np.float32([[f_x, 0.0, c_x],
                                 [0.0, f_y, c_y],
-                                [0.0, 0.0, 1.0] ])
+                                [0.0, 0.0, 1.0]])
     print("Estimated camera matrix: \n" + str(camera_matrix) + "\n")
-    #Distortion coefficients
+    # Distortion coefficients
     camera_distortion = np.float32([0.0, 0.0, 0.0, 0.0, 0.0])
-    #Defining the axes
-    axis = np.float32([[0.0, 0.0, 0.0],
-                       [0.0, 0.0, 0.0],
+    # Defining the axes
+    axis = np.float32([[0.5, 0.0, 0.0],
+                       [0.0, 0.5, 0.0],
                        [0.0, 0.0, 0.5]])
 
-    roll_degree = my_head_pose_estimator.return_roll(image, radians=False)  # Evaluate the roll angle using a CNN
-    pitch_degree = my_head_pose_estimator.return_pitch(image, radians=False)  # Evaluate the pitch angle using a CNN
-    yaw_degree = my_head_pose_estimator.return_yaw(image, radians=False)  # Evaluate the yaw angle using a CNN
-    print("Estimated [roll, pitch, yaw] (degrees) ..... [" + str(roll_degree[0,0,0]) + "," + str(pitch_degree[0,0,0]) + "," + str(yaw_degree[0,0,0])  + "]")
+    # roll_degree = my_head_pose_estimator.return_roll(image, radians=False)  # Evaluate the roll angle using a CNN
+    # pitch_degree = my_head_pose_estimator.return_pitch(image, radians=False)  # Evaluate the pitch angle using a CNN
+    # yaw_degree = my_head_pose_estimator.return_yaw(image, radians=False)  # Evaluate the yaw angle using a CNN
+    # print("Estimated [roll, pitch, yaw] (degrees) ..... [" + str(roll_degree[0, 0, 0]) + "," + str(
+    #     pitch_degree[0, 0, 0]) + "," + str(yaw_degree[0, 0, 0]) + "]")
     roll = my_head_pose_estimator.return_roll(image, radians=True)  # Evaluate the roll angle using a CNN
     pitch = my_head_pose_estimator.return_pitch(image, radians=True)  # Evaluate the pitch angle using a CNN
     yaw = my_head_pose_estimator.return_yaw(image, radians=True)  # Evaluate the yaw angle using a CNN
-    print("Estimated [roll, pitch, yaw] (radians) ..... [" + str(roll[0,0,0]) + "," + str(pitch[0,0,0]) + "," + str(yaw[0,0,0])  + "]")
-    #Getting rotation and translation vector
-    rot_matrix = yaw2rotmat(-yaw[0,0,0]) #Deepgaze use different convention for the Yaw, we have to use the minus sign
+    print("Estimated [roll, pitch, yaw] (radians) ..... [" + str(roll[0, 0, 0]) + "," + str(pitch[0, 0, 0]) + "," + str(
+        yaw[0, 0, 0]) + "]")
+    # Getting rotation and translation vector
+    # rot_matrix = yaw2rotmat(
+    #     -yaw[0, 0, 0])  # Deepgaze use different convention for the Yaw, we have to use the minus sign
 
-    #Attention: OpenCV uses a right-handed coordinates system:
-    #Looking along optical axis of the camera, X goes right, Y goes downward and Z goes forward.
+    # Z-Y-X because that corresponds to yaw, pitch and roll
+    rot_matrix = euler2mat(-yaw[0, 0, 0], pitch[0, 0, 0], roll[0, 0, 0], axes='szyx')
+
+    # Attention: OpenCV uses a right-handed coordinates system:
+    # Looking along optical axis of the camera, X goes right, Y goes downward and Z goes forward.
     rvec, jacobian = cv2.Rodrigues(rot_matrix)
-    tvec = np.array([0.0, 0.0, 1.0], np.float) # translation vector
+    tvec = np.array([0.0, 0.0, 1.0], np.float)  # translation vector
     print(rvec)
 
     imgpts, jac = cv2.projectPoints(axis, rvec, tvec, camera_matrix, camera_distortion)
-    p_start = (int(c_x), int(c_y))
-    p_stop = (int(imgpts[2][0][0]), int(imgpts[2][0][1]))
-    print("point start: " + str(p_start))
-    print("point stop: " + str(p_stop))
-    print("")
 
-    cv2.line(image, p_start, p_stop, (0,0,255), 3) #RED
-    cv2.circle(image, p_start, 1, (0,255,0), 3) #GREEN
+    corner = (int(c_x), int(c_y))
+    cv2.line(image, corner, tuple(imgpts[0].ravel()), (0, 0, 255), 2)
+    cv2.line(image, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 2)
+    cv2.line(image, corner, tuple(imgpts[2].ravel()), (255, 0, 0), 2)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image, 'X', tuple(imgpts[0].ravel()), font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(image, 'Y', tuple(imgpts[1].ravel()), font, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(image, 'Z', tuple(imgpts[2].ravel()), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
 
-    cv2.imwrite(file_save, image)
+    # p_start = (int(c_x), int(c_y))
+    # p_stop = (int(imgpts[2][0][0]), int(imgpts[2][0][1]))
+    # print("point start: " + str(p_start))
+    # print("point stop: " + str(p_stop))
+    # print("")
+    #
+    # cv2.line(image, p_start, p_stop, (0, 0, 255), 3)  # RED
+    # cv2.circle(image, p_start, 1, (0, 255, 0), 3)  # GREEN
 
+    cv2.imshow("Demo", image)
 
+    if cv2.waitKey(1) == 27:
+        break
---
+++ Rotations.py
@@ -0,0 +1,118 @@
+# Code from https://github.com/matthew-brett/transforms3d/blob/master/transforms3d/euler.py
+# Function used to get the rotation matrix
+
+import math
+
+import numpy as np
+
+# axis sequences for Euler angles
+_NEXT_AXIS = [1, 2, 0, 1]
+
+# map axes strings to/from tuples of inner axis, parity, repetition, frame
+_AXES2TUPLE = {
+    'sxyz': (0, 0, 0, 0), 'sxyx': (0, 0, 1, 0), 'sxzy': (0, 1, 0, 0),
+    'sxzx': (0, 1, 1, 0), 'syzx': (1, 0, 0, 0), 'syzy': (1, 0, 1, 0),
+    'syxz': (1, 1, 0, 0), 'syxy': (1, 1, 1, 0), 'szxy': (2, 0, 0, 0),
+    'szxz': (2, 0, 1, 0), 'szyx': (2, 1, 0, 0), 'szyz': (2, 1, 1, 0),
+    'rzyx': (0, 0, 0, 1), 'rxyx': (0, 0, 1, 1), 'ryzx': (0, 1, 0, 1),
+    'rxzx': (0, 1, 1, 1), 'rxzy': (1, 0, 0, 1), 'ryzy': (1, 0, 1, 1),
+    'rzxy': (1, 1, 0, 1), 'ryxy': (1, 1, 1, 1), 'ryxz': (2, 0, 0, 1),
+    'rzxz': (2, 0, 1, 1), 'rxyz': (2, 1, 0, 1), 'rzyz': (2, 1, 1, 1)}
+
+_TUPLE2AXES = dict((v, k) for k, v in _AXES2TUPLE.items())
+
+# For testing whether a number is close to zero
+_EPS4 = np.finfo(float).eps * 4.0
+
+
+def euler2mat(ai, aj, ak, axes='sxyz'):
+    """Return rotation matrix from Euler angles and axis sequence.
+    Parameters
+    ----------
+    ai : float
+        First rotation angle (according to `axes`).
+    aj : float
+        Second rotation angle (according to `axes`).
+    ak : float
+        Third rotation angle (according to `axes`).
+    axes : str, optional
+        Axis specification; one of 24 axis sequences as string or encoded
+        tuple - e.g. ``sxyz`` (the default).
+    Returns
+    -------
+    mat : array-like shape (3, 3) or (4, 4)
+        Rotation matrix or affine.
+    Examples
+    --------
+    >>> R = euler2mat(1, 2, 3, 'syxz')
+    >>> np.allclose(np.sum(R[0]), -1.34786452)
+    True
+    >>> R = euler2mat(1, 2, 3, (0, 1, 0, 1))
+    >>> np.allclose(np.sum(R[0]), -0.383436184)
+    True
+    """
+    try:
+        firstaxis, parity, repetition, frame = _AXES2TUPLE[axes]
+    except (AttributeError, KeyError):
+        _TUPLE2AXES[axes]  # validation
+        firstaxis, parity, repetition, frame = axes
+
+    i = firstaxis
+    j = _NEXT_AXIS[i+parity]
+    k = _NEXT_AXIS[i-parity+1]
+
+    if frame:
+        ai, ak = ak, ai
+    if parity:
+        ai, aj, ak = -ai, -aj, -ak
+
+    si, sj, sk = math.sin(ai), math.sin(aj), math.sin(ak)
+    ci, cj, ck = math.cos(ai), math.cos(aj), math.cos(ak)
+    cc, cs = ci*ck, ci*sk
+    sc, ss = si*ck, si*sk
+
+    M = np.eye(3)
+    if repetition:
+        M[i, i] = cj
+        M[i, j] = sj*si
+        M[i, k] = sj*ci
+        M[j, i] = sj*sk
+        M[j, j] = -cj*ss+cc
+        M[j, k] = -cj*cs-sc
+        M[k, i] = -sj*ck
+        M[k, j] = cj*sc+cs
+        M[k, k] = cj*cc-ss
+    else:
+        M[i, i] = cj*ck
+        M[i, j] = sj*sc-cs
+        M[i, k] = sj*cc+ss
+        M[j, i] = cj*sk
+        M[j, j] = sj*ss+cc
+        M[j, k] = sj*cs-sc
+        M[k, i] = -sj
+        M[k, j] = cj*si
+        M[k, k] = cj*ci
+    return M
+
+
+def yaw2rotmat(yaw):
+    x = 0.0
+    y = 0.0
+    z = yaw
+    ch = np.cos(z)
+    sh = np.sin(z)
+    ca = np.cos(y)
+    sa = np.sin(y)
+    cb = np.cos(x)
+    sb = np.sin(x)
+    rot = np.zeros((3, 3), 'float32')
+    rot[0][0] = ch * ca
+    rot[0][1] = sh * sb - ch * sa * cb
+    rot[0][2] = ch * sa * sb + sh * cb
+    rot[1][0] = sa
+    rot[1][1] = ca * cb
+    rot[1][2] = -ca * sb
+    rot[2][0] = -sh * ca
+    rot[2][1] = sh * sa * cb + ch * sb
+    rot[2][2] = -sh * sa * sb + ch * cb
+    return rot
